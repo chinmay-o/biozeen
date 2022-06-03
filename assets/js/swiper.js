ec97298c7468cc9aed6f8e85db20154ec422f82d
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-06-03 23:57:08 +0530

    beta

---
+++ assets/js/swiper.js
@@ -0,0 +1,34 @@
+var swiperA = new Swiper("#mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 2,
+  breakpoints: {
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    700: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    800: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    900: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 6,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
