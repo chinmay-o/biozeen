49cf1222c2194ac85008c13c5c04404d8f431c87
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-06-09 15:13:03 +0530

    beta

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,31 +17,36 @@ $(document).ready(function(){
 })
 
 // Portfolio Choosing Start
-$('.portfolio-choice h6').click(function(event) {
-
-    var status = $(this).attr('id');
-
-    $("#portfolio-choice01").css("background-color", "#ffffff");
-    $("#portfolio-choice02").css("background-color", "#ffffff");
-    $("#portfolio-choice03").css("background-color", "#ffffff");
-    $(".portfolio-choice h6").css("color", "#000000");
-
-    $("#" + status).css("background-color", "#329d4d");
-    $("#" + status).css("color", "#ffffff");
-
-    if (status == "portfolio-choice01") {
-
-      $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/lab-equipment.jpg)");
-    }
-    else if (status == "portfolio-choice02") {
-
-      $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/support-in-biotech.jpg)");
-    }
-    else if (status == "portfolio-choice03") {
-
-      $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/insight-in-biotech.jpg)");
-    }
-});
+// $('.portfolio-choice h6').click(function(event) {
+//
+//     var status = $(this).attr('id');
+//
+//     $("#portfolio-choice01").css("background-color", "#ffffff");
+//     $("#portfolio-choice02").css("background-color", "#ffffff");
+//     $("#portfolio-choice03").css("background-color", "#ffffff");
+//     $("#portfolio-choice04").css("background-color", "#ffffff");
+//     $(".portfolio-choice h6").css("color", "#000000");
+//
+//     $("#" + status).css("background-color", "#329d4d");
+//     $("#" + status).css("color", "#ffffff");
+//
+//     if (status == "portfolio-choice01") {
+//
+//       $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/lab-equipment.jpg)");
+//     }
+//     else if (status == "portfolio-choice02") {
+//
+//       $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/support-in-biotech.jpg)");
+//     }
+//     else if (status == "portfolio-choice03") {
+//
+//       $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/insight-in-biotech.jpg)");
+//     }
+//     else if (status == "portfolio-choice04") {
+//
+//       $(".portfolio-img").css("background-image", "url(../biozeen/assets/images/insight-in-biotech.jpg)");
+//     }
+// });
 // Portfolio Choosing End
 
 // Animating Counter Start
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -40,6 +40,11 @@ var swiperB = new Swiper("#myTextSwiper", {
 
    delay: 5000,
   },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
   navigation: {
 
     nextElo: '.swiper-button-next',
@@ -79,5 +84,88 @@ var swiperC = new Swiper("#gallerySwiper", {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiperC = new Swiper("#testimonialSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
+
+   delay: 5000,
+  },
+
+  pagination: {
+    el: '.testimonial-pagination',
+  },
+});
+
+var swiperD = new Swiper("#caseStudySwiper", {
+  slidesPerView: 1.5,
+  spaceBetween: 0,
+  autoplay: {
+
+   delay: 5000,
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+    },
+    600: {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+    },
+    700: {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 0,
+    },
+    1200: {
+      slidesPerView: 3.2,
+      spaceBetween: 2,
+    },
+  },
+});
+
+var swiperE = new Swiper("#blogSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
+
+   delay: 5000,
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    700: {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 0,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 2,
+    },
   },
 });
